102c54e6fee3a77617bc356d3a0011cb3ab9f9e8
Hossein Chahabdar <119678031+hosseinch0@users.noreply.github.com>
2023-11-04 10:53:02 +0330

    Array methods added

---
+++ array_replica.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class ArrayReplica
+{
+private:
+    int array[100];
+    int cruiser = 0;
+
+public:
+    void append(int value)
+    {
+        this->array[this->cruiser] = value;
+        cruiser++;
+    }
+
+    void insert(int value, int index)
+    {
+        for (int i = cruiser; i >= index; i--)
+        {
+            this->array[i + 1] = array[i];
+        }
+        this->array[index] = value;
+        this->cruiser++;
+    }
+
+    void display()
+    {
+        sort(this->array);
+        for (int i = 0; i < cruiser; i++)
+        {
+            cout << array[i] << endl;
+        }
+    }
+
+    int del(int index)
+    {
+        for (int i = index; i < cruiser; i++)
+        {
+            this->array[i] = array[i + 1];
+        }
+        cruiser--;
+        return array[index];
+    }
+
+    int find(int value)
+    {
+        for (int i = 0; i < cruiser - 1; i++)
+        {
+            if (value == array[i])
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    int sort(int array[])
+    {
+        for (int i = 0; i < cruiser - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < cruiser - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    this->array[j] = array[j + 1];
+                    this->array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+            {
+                break;
+            }
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    return 0;
+}
